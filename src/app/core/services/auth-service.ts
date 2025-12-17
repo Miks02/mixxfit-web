591a48b0963eb317b053c12a79f6ef9d8f3524bf
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-17 10:46:33 +0100

    feat(service): Add AuthService

---
+++ src/app/core/services/auth-service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { inject } from '@angular/core';
+import { RegisterRequest } from '../models/RegisterRequest';
+import { AuthResponse } from '../models/AuthResponse';
+import { Observable, map, tap } from 'rxjs';
+import { ApiResponse } from '../models/ApiResponse';
+import { UserDto } from '../models/UserDto';
+import { LoginRequest } from '../models/LoginRequest';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+    private readonly api: string = "https://localhost:7263/api";
+    private accessTokenSubject = new BehaviorSubject<string | null>(null);
+    public accessToken$ = this.accessTokenSubject.asObservable();
+
+    private readonly http = inject(HttpClient)
+
+    register(model: RegisterRequest): Observable<UserDto> {
+       return this.http.post<ApiResponse<AuthResponse>>(`${this.api}/auth/register`, model)
+        .pipe(
+            tap(res => this.accessToken = res.data.accessToken),
+            map(res => res.data.user)
+        )
+    }
+
+    login(model: LoginRequest): Observable<UserDto> {
+        return this.http.post<ApiResponse<AuthResponse>>(`${this.api}/auth/login`, model)
+        .pipe(
+            tap(res => this.accessToken = res.data.accessToken),
+            map(res => res.data.user)
+        )
+    }
+
+    get accessToken(): string | null {return this.accessTokenSubject.value}
+    set accessToken(accessToken: string) {this.accessTokenSubject.next(accessToken)}
+
+
+
+}
