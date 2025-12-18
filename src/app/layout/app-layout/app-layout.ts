1a97f5b178f05bd49e6cf85eb459e3f379a00801
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-19 00:27:59 +0100

    refactor(component): Replace <app-dashboard> with router outlet in app layout component

--- src/app/layout/app-layout/app-layout.ts
+++ src/app/layout/app-layout/app-layout.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../utilities/sidebar/sidebar';
 import { Header } from '../utilities/header/header';
-import { Dashboard } from "../../pages/dashboard/dashboard";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-app-layout',
-  imports: [Sidebar, Header, Dashboard],
+  imports: [Sidebar, Header, RouterOutlet],
   templateUrl: './app-layout.html',
   styleUrl: './app-layout.css',
 
