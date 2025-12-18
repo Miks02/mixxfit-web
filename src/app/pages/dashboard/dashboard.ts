4b8cf0c460577494f84323206d49f7fd6a26f35c
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-18 17:47:04 +0100

    feat(page): Add dashboard page component

---
+++ src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { faSolidDumbbell, faSolidFireFlameCurved, faSolidGlassWater, faSolidMoon} from '@ng-icons/font-awesome/solid';
+import { BaseChartDirective } from 'ng2-charts';
+import {
+    ChartConfiguration,
+    ChartOptions,
+    ChartType,
+    Chart, registerables
+} from 'chart.js';
+import { WorkoutsChart } from "../misc/workouts-chart/workouts-chart";
+Chart.register(...registerables)
+
+@Component({
+    selector: 'app-dashboard',
+    imports: [NgIcon, WorkoutsChart],
+    templateUrl: './dashboard.html',
+    styleUrl: './dashboard.css',
+    providers: [provideIcons({faSolidDumbbell, faSolidFireFlameCurved, faSolidGlassWater, faSolidMoon})]
+})
+export class Dashboard {
+
+
+
+}
