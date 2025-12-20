86315db46fafa989cd27afd80222b04b5caa5b59
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-21 00:13:30 +0100

    feat(router): Add dashboard and workout-list routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,38 @@ import { Routes } from '@angular/router';
 import { AuthLayout } from './auth/auth-layout/auth-layout';
 import { Login } from './auth/login/login';
 import { Register } from './auth/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { AppLayout } from './layout/app-layout/app-layout';
+import { WorkoutList } from './pages/workout/workout-list/workout-list';
 
 export const routes: Routes = [
+    {
+        path: "",
+        component: AppLayout,
+        children: [
+            {
+                path: "",
+                redirectTo: "dashboard",
+                pathMatch: "full"
+            },
+            {
+                path: "dashboard",
+                component: Dashboard
+            },
+            {
+                path: "workouts",
+                component: WorkoutList
+            }
+        ]
+    },
     {
         path: "auth",
         component: AuthLayout,
         children: [
+            {
+                path: "",
+                component: Login
+            },
             {
                 path: "login",
                 component: Login
