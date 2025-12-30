ab4de315fb42488b811e432cbaf357cc18a3a3cd
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-30 21:25:21 +0100

    feat(component): Add mock data for workout list component

--- src/app/pages/workout/workout-list/workout-list.ts
+++ src/app/pages/workout/workout-list/workout-list.ts
@@ -1,17 +1,106 @@
 import { Component, inject } from '@angular/core';
 import { LayoutState } from '../../../layout/services/layout-state';
 import { RouterLink } from "@angular/router";
+import { WorkoutService } from '../services/workout-service';
+import { WorkoutDetailsDto } from '../models/WorkoutDetailsDto';
+import { WorkoutListItemDto } from '../models/WorkoutListItemDto';
 
 @Component({
-  selector: 'app-workout-list',
-  imports: [RouterLink],
-  templateUrl: './workout-list.html',
-  styleUrl: './workout-list.css',
+    selector: 'app-workout-list',
+    imports: [RouterLink],
+    templateUrl: './workout-list.html',
+    styleUrl: './workout-list.css',
 })
 export class WorkoutList {
     layoutState = inject(LayoutState)
+    workoutService = inject(WorkoutService)
+
+    workouts: WorkoutDetailsDto[] = []
+    page: number = 1;
+    pageSize: number = 12;
+    workoutList: WorkoutListItemDto[] = this.fillWorkoutList();
+
 
     ngOnInit() {
         this.layoutState.setTitle("Workouts")
+        this.workoutService.getWorkouts()?.subscribe(res => this.workouts = res?.items!);
     }
+
+    getWorkoutCardClass() {
+        if(this.workoutList.length < 6) {
+            return 'w-full'
+        }
+
+        return 'w-full md:w-[calc(50%-0.375rem)]'
+    }
+
+    getSetsBadgeClass(sets: number): string {
+        if (sets >= 28) return 'bg-yellow-400';
+        if (sets >= 24) return 'bg-yellow-400';
+        if (sets >= 20) return 'bg-blue-400';
+        return 'bg-emerald-400';
+    }
+
+    private fillWorkoutList(): WorkoutListItemDto[] {
+        return [
+        {
+            id: 1,
+            name: "Chest/Biceps",
+            exercisesCount: 6,
+            workoutDate: "30.12.2025",
+            setsCount: 24
+        },
+        {
+            id: 2,
+            name: "Back/triceps",
+            exercisesCount: 6,
+            workoutDate: "30.12.2025",
+            setsCount: 28
+        },
+        {
+            id: 3,
+            name: "Shoulders",
+            exercisesCount: 6,
+            workoutDate: "30.12.2025",
+            setsCount: 20
+        },
+        {
+            id: 4,
+            name: "Legs/Abs",
+            exercisesCount: 7,
+            workoutDate: "30.12.2025",
+            setsCount: 30
+        },
+        {
+            id: 5,
+            name: "Chest/Biceps",
+            exercisesCount: 6,
+            workoutDate: "30.12.2025",
+            setsCount: 24
+        },
+        {
+            id:6,
+            name: "Back/triceps",
+            exercisesCount: 6,
+            workoutDate: "30.12.2025",
+            setsCount: 28
+        },
+        {
+            id: 7,
+            name: "Shoulders",
+            exercisesCount: 6,
+            workoutDate: "30.12.2025",
+            setsCount: 20
+        },
+        {
+            id: 8,
+            name: "Legs/Abs",
+            exercisesCount: 7,
+            workoutDate: "30.12.2025",
+            setsCount: 30
+        },
+    ]
+    }
+
+
 }
