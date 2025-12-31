34cceb1e6eb016584f190f2fb088aab23e363b81
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-12-31 11:39:28 +0100

    feat(interceptor): Intercept cloned response and check if it contains status code 401

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,11 +1,14 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpEventType, HttpInterceptorFn } from '@angular/common/http';
 import { AuthService } from '../services/auth-service';
 import { inject } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const authService = inject(AuthService)
-
+    let router = inject(Router)
     let token = authService.accessToken;
+
 
     if(token) {
         const cloned = req.clone({
@@ -13,7 +16,24 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
                 Authorization: `Bearer ${token}`
             }
         });
-        return next(cloned);
+        return next(cloned).pipe(
+            tap(event => {
+                if(event.type === HttpEventType.Response) {
+                    console.log("Returned a response with status: ", event.status);
+                }
+            }),
+            catchError((error: HttpErrorResponse) => {
+                console.log("Error happened ", error.status)
+                if(error.status == 401) {
+                    authService.accessToken = null;
+                    console.log(authService.accessToken);
+                    router.navigate(['/login'])
+
+                }
+                return throwError(() => error);
+
+            })
+        );
     }
 
     return next(req);
