f3b045f2e43e1b338a7180a36678f78004e4259f
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-06 00:28:41 +0100

    feat(service): Switch the workout service implementation from using mock data to using real data

--- src/app/pages/workout/services/workout-service.ts
+++ src/app/pages/workout/services/workout-service.ts
@@ -1,41 +1,56 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of, tap } from 'rxjs';
+import { inject, Injectable, runInInjectionContext } from '@angular/core';
+import { BehaviorSubject, map, Observable, of, tap } from 'rxjs';
 import { Workout } from '../models/Workout';
 import { CreateWorkoutDto } from '../models/CreateWorkoutDto';
+import { PagedResult } from '../../../core/models/PagedResult';
+import { WorkoutDetailsDto } from '../models/WorkoutDetailsDto';
+import { HttpClient } from '@angular/common/http';
+import { WorkoutListItemDto } from '../models/WorkoutListItemDto';
+import { ApiResponse } from '../../../core/models/ApiResponse';
+import { WorkoutPageDto } from '../models/WorkoutPageDto';
+import { WorkoutSummaryDto } from '../models/WorkoutSummaryDto';
 
 @Injectable({
     providedIn: 'root',
 })
 export class WorkoutService {
-    private exercisesSubject = new BehaviorSubject<Workout[]>([]);
-    exercises$ = this.exercisesSubject.asObservable();
+    private readonly api: string = "https://localhost:7263/api";
+    private pagedWorkoutsSubject = new BehaviorSubject<PagedResult<WorkoutListItemDto>>(
+        {
+            items: [],
+            totalCount: 0,
+            page: 1,
+            pageSize: 10
+        }
+    );
+    private workoutSummarySubject = new BehaviorSubject<WorkoutSummaryDto | undefined>(undefined)
+    
+    pagedWorkouts$ = this.pagedWorkoutsSubject.asObservable();
+    workoutSummary$ = this.workoutSummarySubject.asObservable();
 
-    addWorkout(model: CreateWorkoutDto): Observable<CreateWorkoutDto> {
-        return of(model).pipe(
-            tap(workout => {
-                const current = this.exercisesSubject.value
-                const newWorkout: Workout =
-                {
-                    id: this.generateMockWorkoutId(this.exercisesSubject.value),
-                    ...workout,
-                    createdAt: new Date().toISOString()
-                }
+    private http = inject(HttpClient)
 
-                this.exercisesSubject.next([...current, newWorkout])
-            })
+    getUserWorkouts(): Observable<WorkoutPageDto> {
+        return this.http.get<WorkoutPageDto>(`${this.api}/workout/all`).pipe(
+            tap(res => {
+                this.pagedWorkoutsSubject.next(res.pagedWorkouts)
+                this.workoutSummarySubject.next(res.workoutSummary)
+            }),
+            map(res => res)
+        );
+    }
 
+    addWorkout(model: CreateWorkoutDto): Observable<WorkoutDetailsDto> {
+        return this.http.post<WorkoutDetailsDto>(`${this.api}/workout`, model).pipe(
+            tap(res => console.log(res))
         )
 
     }
 
-    getWorkouts(): Observable<Workout[]> {
-        return this.exercises$;
+    deleteWorkout(id: number): Observable<ApiResponse<void>> {
+        return this.http.delete<ApiResponse<void>>(`${this.api}/workout/delete/${id}`).pipe(
+            tap(res => console.log("Delete response: ", res))
+        )
     }
-
-    private generateMockWorkoutId(workouts: Workout[]) {
-        return workouts.length ? Math.max(...workouts.map(w => w.id)) + 1 : 1
-    }
-
-
 
 }
