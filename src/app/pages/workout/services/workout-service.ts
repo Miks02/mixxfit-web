d4207558373bfc47d73fa356b9cee1918896b511
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-07 14:15:35 +0100

    feat(service): Add a method for retrieveing user workouts only, without the summary and clean up the code for better readability

--- src/app/pages/workout/services/workout-service.ts
+++ src/app/pages/workout/services/workout-service.ts
@@ -1,14 +1,14 @@
-import { inject, Injectable, runInInjectionContext } from '@angular/core';
-import { BehaviorSubject, map, Observable, of, tap } from 'rxjs';
-import { Workout } from '../models/Workout';
+import { inject, Injectable, } from '@angular/core';
+import { BehaviorSubject, map, Observable, tap } from 'rxjs';
 import { CreateWorkoutDto } from '../models/CreateWorkoutDto';
 import { PagedResult } from '../../../core/models/PagedResult';
 import { WorkoutDetailsDto } from '../models/WorkoutDetailsDto';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { WorkoutListItemDto } from '../models/WorkoutListItemDto';
 import { ApiResponse } from '../../../core/models/ApiResponse';
 import { WorkoutPageDto } from '../models/WorkoutPageDto';
 import { WorkoutSummaryDto } from '../models/WorkoutSummaryDto';
+import { QueryParams } from '../../../core/models/QueryParams';
 
 @Injectable({
     providedIn: 'root',
@@ -16,29 +16,43 @@ import { WorkoutSummaryDto } from '../models/WorkoutSummaryDto';
 export class WorkoutService {
     private readonly api: string = "https://localhost:7263/api";
     private pagedWorkoutsSubject = new BehaviorSubject<PagedResult<WorkoutListItemDto>>(
-        {
-            items: [],
-            totalCount: 0,
-            page: 1,
-            pageSize: 10
-        }
+        {items: [], totalCount: 0, page: 1, pageSize: 12}
     );
     private workoutSummarySubject = new BehaviorSubject<WorkoutSummaryDto | undefined>(undefined)
+    private queryParams = new BehaviorSubject<QueryParams>(
+        {sort: "", search: "", date: "", page: 1}
+    )
 
     pagedWorkouts$ = this.pagedWorkoutsSubject.asObservable();
     workoutSummary$ = this.workoutSummarySubject.asObservable();
 
     private http = inject(HttpClient)
 
-    getUserWorkouts(): Observable<WorkoutPageDto> {
-        return this.http.get<WorkoutPageDto>(`${this.api}/workout/all`).pipe(
+    getQueryParams() {return this.queryParams.value}
+    setQueryParams(queryParams: QueryParams) {this.queryParams.next(queryParams)}
+
+    getUserWorkoutsPage(): Observable<WorkoutPageDto> {
+        const params = this.getHttpQueryParams();
+
+        return this.http.get<ApiResponse<WorkoutPageDto>>(`${this.api}/workout/overview`, {params}).pipe(
             tap(res => {
-                this.pagedWorkoutsSubject.next(res.pagedWorkouts)
-                this.workoutSummarySubject.next(res.workoutSummary)
-                console.log(res.workoutSummary)
+                this.pagedWorkoutsSubject.next(res.data.pagedWorkouts)
+                this.workoutSummarySubject.next(res.data.workoutSummary)
             }),
-            map(res => res)
+            map(res => res.data)
         );
+    }
+
+    getUserWorkoutsByQuery(): Observable<PagedResult<WorkoutListItemDto>> {
+        const params = this.getHttpQueryParams();
+
+        return this.http.get<ApiResponse<PagedResult<WorkoutListItemDto>>>(`${this.api}/workout`, {params})
+            .pipe(
+                tap(res => {
+                     this.pagedWorkoutsSubject.next(res.data)
+                }),
+                 map(res => res.data)
+            )
     }
 
     addWorkout(model: CreateWorkoutDto): Observable<WorkoutDetailsDto> {
@@ -52,6 +66,16 @@ export class WorkoutService {
         return this.http.delete<ApiResponse<void>>(`${this.api}/workout/delete/${id}`).pipe(
             tap(res => console.log("Delete response: ", res))
         )
+    }
+
+    private getHttpQueryParams(): HttpParams {
+        const queryParams = this.getQueryParams();
+
+        return new HttpParams()
+            .set('sortBy', queryParams.sort)
+            .set('searchBy', queryParams.search)
+            .set('date', queryParams.date)
+            .set('page', queryParams.page)
     }
 
 }
