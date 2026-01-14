56be8dd482220eb4179fd8f6620be2a2815f879c
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-14 17:06:56 +0100

    feat(factory): Add factory functions for creating forms that will be used for updating user data via patch request

--- src/app/core/helpers/Factories.ts
+++ src/app/core/helpers/Factories.ts
@@ -59,3 +59,52 @@ export function createWorkoutObject(form: FormGroup): CreateWorkoutDto {
         }))
     }
 }
+
+export function createFullNameForm(fb: FormBuilder, firstName: string = '', lastName: string = ''): FormGroup {
+    return fb.group({
+        firstName: [firstName, [Validators.required, Validators.minLength(2), Validators.maxLength(20)]],
+        lastName: [lastName, [Validators.required, Validators.minLength(2), Validators.maxLength(20)]]
+    });
+}
+
+export function createDateOfBirthForm(fb: FormBuilder, dateOfBirth: string = ''): FormGroup {
+    return fb.group({
+        dateOfBirth: [dateOfBirth, Validators.required]
+    });
+}
+
+export function createUsernameForm(fb: FormBuilder, username: string = ''): FormGroup {
+    return fb.group({
+        username: [username, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]]
+    });
+}
+
+export function createEmailForm(fb: FormBuilder, email: string = ''): FormGroup {
+    return fb.group({
+        email: [email, [Validators.required, Validators.email]]
+    });
+}
+
+export function createGenderForm(fb: FormBuilder, gender: number | null = null): FormGroup {
+    return fb.group({
+        gender: [gender, Validators.required]
+    });
+}
+
+export function createWeightForm(fb: FormBuilder, weight: number | null = null): FormGroup {
+    return fb.group({
+        weight: [weight, [Validators.required, Validators.min(24), Validators.max(399)]]
+    });
+}
+
+export function createHeightForm(fb: FormBuilder, height: number | null = null): FormGroup {
+    return fb.group({
+        height: [height, [Validators.required, Validators.min(69), Validators.max(249)]]
+    });
+}
+
+export function createProfilePictureForm(fb: FormBuilder): FormGroup {
+    return fb.group({
+        profileImage: ['']
+    });
+}
