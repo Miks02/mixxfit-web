ed01317776dd55e698d5682253cff02381afe676
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-16 15:23:00 +0100

    styles(component): Reorganize the layout of the dashboard component and improve the UI
    - Add weight tracking line chart
    - Add quick actions section
    - Redesign the right panel with user's information
    - Add last 10 workouts section

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,30 +1,38 @@
-import { Component, inject, runInInjectionContext } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { faSolidDumbbell, faSolidFireFlameCurved, faSolidGlassWater, faSolidMoon} from '@ng-icons/font-awesome/solid';
-import { BaseChartDirective } from 'ng2-charts';
+import { faSolidDumbbell, faSolidFireFlameCurved, faSolidGlassWater, faSolidMoon, faSolidScaleUnbalanced, faSolidUtensils, faSolidCalculator } from '@ng-icons/font-awesome/solid';
 import {
-    ChartConfiguration,
-    ChartOptions,
-    ChartType,
     Chart, registerables
 } from 'chart.js';
 import { WorkoutsChart } from "../misc/workouts-chart/workouts-chart";
 import { LayoutState } from '../../layout/services/layout-state';
+import { AuthService } from '../../core/services/auth-service';
+import { Subject, takeUntil } from 'rxjs';
+import { WeightChart } from "../misc/weight-chart/weight-chart";
+import { RouterLink } from "@angular/router";
+
 Chart.register(...registerables)
 
 @Component({
     selector: 'app-dashboard',
-    imports: [NgIcon, WorkoutsChart],
+    imports: [NgIcon, WorkoutsChart, WeightChart, RouterLink],
     templateUrl: './dashboard.html',
     styleUrl: './dashboard.css',
-    providers: [provideIcons({faSolidDumbbell, faSolidFireFlameCurved, faSolidGlassWater, faSolidMoon})]
+    providers: [provideIcons({faSolidDumbbell, faSolidFireFlameCurved, faSolidGlassWater, faSolidMoon, faSolidScaleUnbalanced, faSolidUtensils, faSolidCalculator})]
 })
 export class Dashboard {
-
     layoutState = inject(LayoutState)
+
+    private destroy$ = new Subject<void>();
+
 
     ngOnInit() {
         this.layoutState.setTitle("Dashboard")
+    }
+
+    ngOnDestroy() {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 
 }
