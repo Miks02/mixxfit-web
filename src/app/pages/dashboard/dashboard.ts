be3f0dd4e9233c681ad01b44c8b7f4197138e43e
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-17 23:49:14 +0100

    feat(component): Implement user$ and dashboard$ signals that will be used in the component for showing the current up to date information in dashboard component

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -7,32 +7,39 @@ import {
 import { WorkoutsChart } from "../misc/workouts-chart/workouts-chart";
 import { LayoutState } from '../../layout/services/layout-state';
 import { AuthService } from '../../core/services/auth-service';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, take, takeUntil } from 'rxjs';
 import { WeightChart } from "../misc/weight-chart/weight-chart";
 import { RouterLink } from "@angular/router";
-
+import { DashboardState } from './services/dashboard-state';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { UserService } from '../../core/services/user-service';
+import { DatePipe } from '@angular/common';
 Chart.register(...registerables)
 
 @Component({
     selector: 'app-dashboard',
-    imports: [NgIcon, WorkoutsChart, WeightChart, RouterLink],
+    imports: [NgIcon, WorkoutsChart, WeightChart, RouterLink, DatePipe],
     templateUrl: './dashboard.html',
     styleUrl: './dashboard.css',
     providers: [provideIcons({faSolidDumbbell, faSolidFireFlameCurved, faSolidGlassWater, faSolidMoon, faSolidScaleUnbalanced, faSolidUtensils, faSolidCalculator})]
 })
 export class Dashboard {
-    layoutState = inject(LayoutState)
+    layoutState = inject(LayoutState);
+    dashboardState = inject(DashboardState);
+    userService = inject(UserService);
 
-    private destroy$ = new Subject<void>();
-
+    dashboard$ = toSignal(this.dashboardState.dashboard$, {initialValue: null})
+    user$ = toSignal(this.userService.userDetails$, {initialValue: null})
 
     ngOnInit() {
         this.layoutState.setTitle("Dashboard")
+        this.loadDashboard();
     }
 
-    ngOnDestroy() {
-        this.destroy$.next();
-        this.destroy$.complete();
+    loadDashboard() {
+        return this.dashboardState.getDashboard()
+        .pipe(take(1))
+        .subscribe();
     }
 
 }
