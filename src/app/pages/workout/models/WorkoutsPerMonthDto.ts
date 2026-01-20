e70dd7c5eaf56c31911d5436b7d3b01f4c4a581a
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-20 23:38:40 +0100

    feat(dto): Add WorkoutsPerMontDto
    - This data transfer object will be used mostly for charts (or only for charts)

---
+++ src/app/pages/workout/models/WorkoutsPerMonthDto.ts
@@ -0,0 +1,15 @@
+export type WorkoutsPerMonthDto = {
+    years: number[],
+    januaryWorkouts: number;
+    februaryWorkouts: number;
+    marchWorkouts: number;
+    aprilWorkouts: number;
+    mayWorkouts: number;
+    juneWorkouts: number;
+    julyWorkouts: number;
+    augustWorkouts: number;
+    septemberWorkouts: number;
+    octoberWorkouts: number;
+    novemberWorkouts: number;
+    decemberWorkouts: number;
+};
