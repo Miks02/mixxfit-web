88761cc29b5666d3a1af1694d459f94e26d855b8
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-27 23:25:21 +0100

    feat(service): Add query params to the get methods in weight-entry-service

--- src/app/pages/weight/services/weight-entry-service.ts
+++ src/app/pages/weight/services/weight-entry-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { UserService } from '../../../core/services/user-service';
 import { WeightCreateRequestDto } from '../models/WeightCreateRequestDto';
@@ -7,6 +7,7 @@ import { WeightEntryDetailsDto } from '../models/WeightEntryDetailsDto';
 import { BehaviorSubject, tap } from 'rxjs';
 import { WeightSummaryDto } from '../models/WeightSummaryDto';
 import { WeightRecordDto } from '../models/WeightRecordDto';
+import { WeightListDetailsDto } from '../models/WeightListDetailsDto';
 
 @Injectable({
   providedIn: 'root',
@@ -18,36 +19,46 @@ export class WeightEntryService {
     private userService = inject(UserService);
 
     private weightSummarySubject = new BehaviorSubject<WeightSummaryDto | undefined>(undefined);
-    private weightLogsSubject = new BehaviorSubject<WeightRecordDto[] | undefined>(undefined);
+    private weightListDetailsSubject = new BehaviorSubject<WeightListDetailsDto | undefined>(undefined);
 
     weightSummary$ = this.weightSummarySubject.asObservable();
-    weightLogs$ = this.weightLogsSubject.asObservable();
+    weightListDetails$ = this.weightListDetailsSubject.asObservable();
 
     set weightSummary(summary: Partial<WeightSummaryDto>) {
         const current = this.weightSummarySubject.getValue() as WeightSummaryDto;
         this.weightSummarySubject.next({...current, ...summary});
     }
 
-    getMyWeightSummary() {
-        return this.http.get<WeightSummaryDto>(`${this.api}/weight-entries`)
+    getMyWeightSummary(month: number | null = null, year: number | null = null) {
+        const params = new HttpParams()
+            .set('month', month as number)
+            .set('year', year as number)
+
+        return this.http.get<WeightSummaryDto>(`${this.api}/weight-entries`, {params})
             .pipe(
                 tap(res => {
                     this.weightSummary = {
                         firstEntry: res.firstEntry,
                         currentWeight: res.currentWeight,
-                        progress: res.progress
+                        progress: res.progress,
+                        years: res.years
                     }
-                    this.weightLogsSubject.next(res.weightLogs);
+                    this.weightListDetailsSubject.next(res.weightListDetails);
                     this.userService.userDetails = {currentWeight: res.currentWeight.weight};
                 })
             )
     }
 
-    getMyWeightLogs() {
-        return this.http.get<WeightRecordDto[]>(`${this.api}/weight-entries/logs`)
+    getMyWeightLogs(month: number | null = null, year: number | null = null) {
+        const params = new HttpParams()
+            .set('month', month as number)
+            .set('year', year as number)
+
+
+        return this.http.get<WeightListDetailsDto>(`${this.api}/weight-entries/logs`, {params})
             .pipe(
                 tap(res => {
-                    this.weightLogsSubject.next(res);
+                    this.weightListDetailsSubject.next(res);
                 })
             );
     }
