8de048fc7a4b44019192e8c0a5b628b99ff63ab4
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-02-04 01:16:04 +0100

    feat(env): Remove full urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: "https://vitalops-api.onrender.com/api",
-    urlOnly: "https://vitalops-api.onrender.com/"
+    apiUrl: "/api",
+    urlOnly: "/"
 };
