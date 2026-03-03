2cb1ee855dd6dc40e8d4aa86345a37e3beaf6299
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-03 21:30:35 +0100

    feat(nutrition): Add a nutrition-service class
    - Currently contains only the simple calculateCalories method

---
+++ src/app/pages/nutrition/services/nutrition-service.ts
@@ -0,0 +1,25 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { CalorieResult } from '../models/CalorieResult';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from '../../../core/services/user-service';
+import { SetDailyCaloriesRequest } from '../models/SetDailyCaloriesRequest';
+@Injectable({
+    providedIn: 'root',
+})
+export class NutritionService {
+    private api: string = environment.apiUrl;
+
+    private http = inject(HttpClient);
+    private userService = inject(UserService);
+
+    private calorieResultSubject = new BehaviorSubject<CalorieResult | undefined>(undefined);
+
+    calculateCalories(request: SetDailyCaloriesRequest): Observable<CalorieResult>  {
+        return this.http.post<CalorieResult>(`${this.api}/calories`, request).pipe(
+            tap((res) => console.log(res))
+        )
+    }
+
+}
