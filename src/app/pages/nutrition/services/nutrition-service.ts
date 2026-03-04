4bb7a3f420d02a5026ef1b4d8712942a87515e56
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-04 22:12:40 +0100

    feat(calorie-calculator): update daily calorie goal handling and add it's result to the dashboard card

--- src/app/pages/nutrition/services/nutrition-service.ts
+++ src/app/pages/nutrition/services/nutrition-service.ts
@@ -5,21 +5,26 @@ import { CalorieResult } from '../models/CalorieResult';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from '../../../core/services/user-service';
 import { SetDailyCaloriesRequest } from '../models/SetDailyCaloriesRequest';
+
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class NutritionService {
-    private api: string = environment.apiUrl + '/nutrition';
+  private api: string = environment.apiUrl + '/nutrition';
 
-    private http = inject(HttpClient);
-    private userService = inject(UserService);
+  private http = inject(HttpClient);
+  private userService = inject(UserService);
 
-    private calorieResultSubject = new BehaviorSubject<CalorieResult | undefined>(undefined);
+  calculateCalories(request: SetDailyCaloriesRequest): Observable<CalorieResult>  {
+    return this.http.post<CalorieResult>(`${this.api}/calorie-goals`, request)
+  }
 
-    calculateCalories(request: SetDailyCaloriesRequest): Observable<CalorieResult>  {
-        return this.http.post<CalorieResult>(`${this.api}/calorie-goals`, request)
-    }
-
-
+  setDailyCalories(request: number | null): Observable<{calories: number}> {
+    return this.http.post<{calories: number}>(`${this.api}/daily-calories`, { calories: request }).pipe(
+      tap((result) => {
+        this.userService.userDetails.dailyCalorieGoal = result.calories;
+      })
+    );
+  }
 
 }
