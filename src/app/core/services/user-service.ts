00fe828af77d7d2db88acff49a1b37f7d21c17b6
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-05 18:55:00 +0100

    feat(user-state): Add a user-state service that will be responsible for holding value of a logged in user

--- src/app/core/services/user-service.ts
+++ src/app/core/services/user-service.ts
@@ -11,6 +11,8 @@ import { UpdateGenderDto } from '../models/User/UpdateGenderDto';
 import { UpdateHeightDto } from '../models/User/UpdateHeightDto';
 import { UpdateTargetWeightDto } from '../models/User/UpdateTargetWeightDto';
 import { Gender } from '../models/Gender';
+import { UserState } from '../states/user-state';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable({
     providedIn: 'root',
@@ -22,6 +24,7 @@ export class UserService {
     public userDetails$ = this.userDetailsSubject.asObservable();
 
     private http = inject(HttpClient);
+    private userState = inject(UserState);
 
     set userDetails(userDetails: Partial<UserDetailsDto>) {
         const currentUser = this.userDetailsSubject.getValue() as UserDetailsDto
@@ -32,39 +35,26 @@ export class UserService {
         return this.userDetailsSubject.getValue() as UserDetailsDto;
     }
 
-    get isUserLoaded() {
-        return this.userDetailsSubject.getValue() !== null;
-    }
-
-    private mergeUserDetails<T extends Partial<UserDetailsDto>>(patch: T): UserDetailsDto {
-        const current = this.userDetailsSubject.getValue() ?? {} as UserDetailsDto;
-        const next = { ...current, ...patch } as UserDetailsDto;
-        return next;
-    }
-
     resetCurrentUser() {
         this.userDetailsSubject.next(null);
     }
 
     getMe() {
-        if(!this.isUserLoaded) {
-            return this.http.get<UserDetailsDto>(`${this.api}/users/me`)
-            .pipe(
-                tap((res) => {
-                    this.userDetails = res;
-                    if(res.imagePath !== null)
-                        this.userDetails.imagePath = this.urlOnly + res.imagePath;
-                })
-            )
+
+        if(this.userState.isUserLoaded()) {
+            const userDetails = this.userState.userDetails() as UserDetailsDto;
+            return of(userDetails);
         }
-        return of(this.userDetailsSubject.getValue() as UserDetailsDto)
-        .pipe(
+
+        return this.http.get<UserDetailsDto>(`${this.api}/users/me`).pipe(
             tap((res) => {
-                this.userDetails = res;
-                if(res.imagePath !== null)
-                    this.userDetails.imagePath = this.urlOnly + res.imagePath;
+                this.userState.setUserDetails(res);
+                if(res.imagePath !== null) {
+                    let image = this.urlOnly + res.imagePath;
+                    this.userState.updateUserDetails({ imagePath: image });
+                }
             })
-        );
+        )
     }
 
     deleteAccount() {
@@ -75,8 +65,7 @@ export class UserService {
         return this.http.delete<void>(`${this.api}/users/profile-picture`)
         .pipe(
             tap(() => {
-                const next = this.mergeUserDetails({imagePath: null});
-                this.userDetailsSubject.next(next)
+                this.userState.updateUserDetails({imagePath: null});
             })
         )
     }
@@ -85,10 +74,8 @@ export class UserService {
         return this.http.patch<UpdateFullNameDto>(`${this.api}/users/fullname`, fullName )
         .pipe(
             tap((res) => {
-                const next = this.mergeUserDetails({fullName: res.firstName + ' ' + res.lastName});
-                this.userDetailsSubject.next(next)
-            }),
-            tap((res) => { console.log(res) })
+                this.userState.updateUserDetails({fullName: res.firstName + ' ' + res.lastName})
+            })
         )
     }
 
@@ -96,8 +83,7 @@ export class UserService {
         return this.http.patch(`${this.api}/users/username`, username, {responseType: 'text'})
         .pipe(
             tap(res => {
-                const next = this.mergeUserDetails({ userName: res });
-                this.userDetailsSubject.next(next);
+                this.userState.updateUserDetails({userName: res});
             })
         );
     }
@@ -106,8 +92,7 @@ export class UserService {
         return this.http.patch(`${this.api}/users/email`, email, {responseType: 'text'})
         .pipe(
             tap(res => {
-                const next = this.mergeUserDetails({ email: res });
-                this.userDetailsSubject.next(next);
+                this.userState.updateUserDetails({email: res});
             })
         );
     }
@@ -116,8 +101,7 @@ export class UserService {
         return this.http.patch<string>(`${this.api}/users/date-of-birth`, dob)
         .pipe(
             tap(res => {
-                const next = this.mergeUserDetails({ dateOfBirth: res });
-                this.userDetailsSubject.next(next);
+                this.userState.updateUserDetails({dateOfBirth: res});
             })
         );
     }
@@ -126,8 +110,7 @@ export class UserService {
         return this.http.patch<Gender>(`${this.api}/users/gender`, gender)
         .pipe(
             tap(res => {
-                const next = this.mergeUserDetails({ gender: res });
-                this.userDetailsSubject.next(next);
+                this.userState.updateUserDetails({gender: res});
             })
         );
     }
@@ -136,8 +119,7 @@ export class UserService {
         return this.http.patch<number>(`${this.api}/users/height`, height)
         .pipe(
             tap(res => {
-                const next = this.mergeUserDetails({ height: res });
-                this.userDetailsSubject.next(next);
+                this.userState.updateUserDetails({height: res});
             })
         );
     }
@@ -146,8 +128,7 @@ export class UserService {
         return this.http.patch<number>(`${this.api}/users/target-weight`, targetWeight)
         .pipe(
             tap(res => {
-                const next = this.mergeUserDetails({ targetWeight: res });
-                this.userDetailsSubject.next(next);
+                this.userState.updateUserDetails({targetWeight: res});
             })
         );
     }
@@ -159,8 +140,7 @@ export class UserService {
         return this.http.patch(`${this.api}/users/profile-picture`, formData, {responseType: 'text'})
         .pipe(
             tap(res => {
-                const next = this.mergeUserDetails({imagePath: this.urlOnly + res})
-                this.userDetailsSubject.next(next);
+                this.userState.updateUserDetails({imagePath: this.urlOnly + res});
             })
         )
     }
---
+++ src/app/core/states/user-state.ts
@@ -0,0 +1,29 @@
+import { computed, Injectable, signal, Signal, WritableSignal } from '@angular/core';
+import { UserDetailsDto } from '../models/UserDetailsDto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserState {
+  private _userDetails: WritableSignal<UserDetailsDto | null> = signal(null);
+
+  readonly userDetails: Signal<UserDetailsDto | null> = this._userDetails.asReadonly();
+
+  setUserDetails(userDetails: UserDetailsDto) {
+    this._userDetails.set(userDetails);
+  }
+
+  updateUserDetails(userDetails: Partial<UserDetailsDto>) {
+    this._userDetails.update((current) => ({...current,...userDetails} as UserDetailsDto));
+  }
+
+  resetCurrentUser() {
+    this._userDetails.set(null);
+  }
+
+  isUserLoaded() {
+    return this._userDetails() !== null;
+  }
+
+
+}
