207163af5b138dd52427e23a24b148be77d692f7
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-05 22:49:31 +0100

    feat(component): Add calorie-summary component
    - It will be used to review the output of calculating calories from its parent component 'calorie-calculator'
    - It will also be responsible for sending the selected daily caloric value to the parent component

---
+++ src/app/features/nutrition/components/calories-summary/calories-summary.ts
@@ -0,0 +1,49 @@
+import { Component, computed, input, model, output, signal } from '@angular/core';
+import { CalorieResult } from '../../models/CalorieResult';
+import { NgIcon, provideIcons } from "@ng-icons/core";
+import { faSolidArrowDown, faSolidArrowLeft, faSolidArrowUp, faSolidBoltLightning, faSolidCalculator, faSolidCheck, faSolidFireFlameCurved, faSolidHourglass, faSolidMinus, faSolidPerson, faSolidRulerVertical, faSolidScaleUnbalanced, faSolidXmark } from '@ng-icons/font-awesome/solid';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calories-summary',
+  imports: [NgIcon, FormsModule],
+  templateUrl: './calories-summary.html',
+  styleUrl: './calories-summary.css',
+  providers: [provideIcons({
+    faSolidXmark, faSolidCalculator, faSolidFireFlameCurved,
+    faSolidScaleUnbalanced, faSolidPerson, faSolidRulerVertical,
+    faSolidHourglass, faSolidBoltLightning, faSolidArrowDown,
+    faSolidArrowUp, faSolidMinus, faSolidCheck, faSolidArrowLeft
+  })]
+})
+export class CaloriesSummary {
+    result = input.required<CalorieResult>();
+
+    back = output<void>();
+    updateDailyCalories = output<number>();
+
+    selectedCalorieValue = model<number | null>(null);
+
+    onBack() {
+        this.back.emit();
+    }
+
+    onUpdateDailyCalories() {
+        const calories = this.selectedCalorieValue();
+        if(!calories)
+            return;
+
+        this.updateDailyCalories.emit(calories);
+    }
+
+    setSelectedCalories(calories: number) {
+        this.selectedCalorieValue.set(calories);
+    }
+
+    areCaloriesValid = computed(() => {
+        const calories = this.selectedCalorieValue();
+        return calories !== null && (calories > 1000 && calories < 10000);
+    });
+
+
+}
