269ab78cf7078e0afff64520a3bcc89dedc27896
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-07 21:21:01 +0100

    refactor(workout-details, workout-service): Reorder imports for consistency and readability

--- src/app/features/workout/services/workout-service.ts
+++ src/app/features/workout/services/workout-service.ts
@@ -12,100 +12,93 @@ import { WorkoutsPerMonthDto } from '../models/workouts-per-month-dto';
 import { environment } from '../../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class WorkoutService {
-  private readonly api: string = environment.apiUrl;
-  private pagedWorkoutsSubject = new BehaviorSubject<PagedResult<WorkoutListItemDto> | undefined>(undefined)
-  private workoutSummarySubject = new BehaviorSubject<WorkoutSummaryDto | undefined>(undefined)
-  private queryParams = new BehaviorSubject<QueryParams | undefined>(undefined)
-  private workoutCountsSubject = new BehaviorSubject<WorkoutsPerMonthDto | null>(null)
+    private readonly api: string = environment.apiUrl;
+    private pagedWorkoutsSubject = new BehaviorSubject<PagedResult<WorkoutListItemDto> | undefined>(undefined)
+    private workoutSummarySubject = new BehaviorSubject<WorkoutSummaryDto | undefined>(undefined)
+    private queryParams = new BehaviorSubject<QueryParams | undefined>(undefined)
+    private workoutCountsSubject = new BehaviorSubject<WorkoutsPerMonthDto | null>(null)
 
-  pagedWorkouts$ = this.pagedWorkoutsSubject.asObservable();
-  workoutSummary$ = this.workoutSummarySubject.asObservable();
-  workoutCounts$ = this.workoutCountsSubject.asObservable();
+    pagedWorkouts$ = this.pagedWorkoutsSubject.asObservable();
+    workoutSummary$ = this.workoutSummarySubject.asObservable();
+    workoutCounts$ = this.workoutCountsSubject.asObservable();
 
-  private http = inject(HttpClient)
+    private http = inject(HttpClient)
 
-  getQueryParams() {return this.queryParams.value}
-  setQueryParams(queryParams: QueryParams) {this.queryParams.next(queryParams)}
+    getQueryParams() {return this.queryParams.value}
+    setQueryParams(queryParams: QueryParams) {this.queryParams.next(queryParams)}
 
-  getUserWorkoutsPage(): Observable<WorkoutPageDto> {
-    const params = this.getHttpQueryParams();
+    getUserWorkoutsPage(): Observable<WorkoutPageDto> {
+        const params = this.getHttpQueryParams();
 
-    return this.http.get<WorkoutPageDto>(`${this.api}/workouts/overview`, {params}).pipe(
-      tap(res => {
-        this.pagedWorkoutsSubject.next(res.pagedWorkouts)
-        this.workoutSummarySubject.next(res.workoutSummary)
-      }),
-      map(res => res)
-    );
-  }
-
-  getUserWorkoutsByQuery(): Observable<PagedResult<WorkoutListItemDto>> {
-    const params = this.getHttpQueryParams();
-
-    return this.http.get<PagedResult<WorkoutListItemDto>>(`${this.api}/workouts`, {params})
-    .pipe(
-      tap(res => {
-        this.pagedWorkoutsSubject.next(res)
-      }),
-      map(res => res)
-    )
-  }
-
-  getUserWorkout(id: number): Observable<WorkoutDetailsDto> {
-    return this.http.get<WorkoutDetailsDto>(`${this.api}/workouts/${id}`);
-  }
-
-  getUserWorkoutCountsByMonth(year: number | null = null) {
-    let params = new HttpParams();
-
-    if (year !== null && year !== undefined) {
-      params = params.set('year', year.toString());
+        return this.http.get<WorkoutPageDto>(`${this.api}/workouts/overview`, {params}).pipe(
+            tap(res => {
+                this.pagedWorkoutsSubject.next(res.pagedWorkouts)
+                this.workoutSummarySubject.next(res.workoutSummary)
+            }),
+            map(res => res)
+        );
     }
 
-    return this.http.get<WorkoutsPerMonthDto>(`${this.api}/workouts/workout-chart`, { params })
-    .pipe(
-      tap(res => this.workoutCountsSubject.next(res))
-    );
-  }
+    getUserWorkoutsByQuery(): Observable<PagedResult<WorkoutListItemDto>> {
+        const params = this.getHttpQueryParams();
 
-  addWorkout(model: CreateWorkoutDto): Observable<WorkoutDetailsDto> {
-    return this.http.post<WorkoutDetailsDto>(`${this.api}/workouts`, model).pipe(
-      tap(res => console.log(res))
-    )
+        return this.http.get<PagedResult<WorkoutListItemDto>>(`${this.api}/workouts`, {params})
+        .pipe(
+            tap(res => {
+                this.pagedWorkoutsSubject.next(res)
+            }),
+            map(res => res)
+        )
+    }
 
-  }
+    getUserWorkout(id: number): Observable<WorkoutDetailsDto> {
+        return this.http.get<WorkoutDetailsDto>(`${this.api}/workouts/${id}`);
+    }
 
-  deleteWorkout(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.api}/workouts/${id}`).pipe(
-      tap(res => console.log("Delete response: ", res))
-    )
-  }
+    getUserWorkoutCountsByMonth(year: number | null = null) {
+        let params = new HttpParams();
 
-  private getHttpQueryParams(): HttpParams {
-    const queryParams = this.getQueryParams();
-    let params = new HttpParams();
+        if (year !== null && year !== undefined) {
+            params = params.set('year', year.toString());
+        }
 
-    console.log("Current query params: ", queryParams);
+        return this.http.get<WorkoutsPerMonthDto>(`${this.api}/workouts/workout-chart`, { params })
+        .pipe(
+            tap(res => this.workoutCountsSubject.next(res))
+        );
+    }
 
-    if(queryParams === undefined)
-      return params;
+    addWorkout(model: CreateWorkoutDto): Observable<WorkoutDetailsDto> {
+        return this.http.post<WorkoutDetailsDto>(`${this.api}/workouts`, model)
+    }
 
-    if(queryParams.page !== null && queryParams.page !== undefined)
-      params = params.set('page', queryParams.page)
+    deleteWorkout(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.api}/workouts/${id}`)
+    }
 
-    if(queryParams.sort !== null && queryParams.sort !== undefined)
-      params = params.set('sort', queryParams.sort);
+    private getHttpQueryParams(): HttpParams {
+        const queryParams = this.getQueryParams();
+        let params = new HttpParams();
+        
+        if(queryParams === undefined)
+            return params;
 
-    if(queryParams.search !== null && queryParams.search !== undefined)
-      params = params.set('search', queryParams.search);
+        if(queryParams.page !== null && queryParams.page !== undefined)
+            params = params.set('page', queryParams.page)
 
-    if(queryParams.date !== null && queryParams.date !== undefined)
-      params = params.set('date', queryParams.date);
+        if(queryParams.sort !== null && queryParams.sort !== undefined)
+            params = params.set('sort', queryParams.sort);
 
-    return params;
-  }
+        if(queryParams.search !== null && queryParams.search !== undefined)
+            params = params.set('search', queryParams.search);
+
+        if(queryParams.date !== null && queryParams.date !== undefined)
+            params = params.set('date', queryParams.date);
+
+        return params;
+    }
 
 }
