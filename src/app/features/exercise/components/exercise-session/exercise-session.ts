9bb62c3de9e5e49fdd68dc8635cc4f07496ec1ec
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-30 16:09:35 +0200

    styles(exercise-session): Add slight improvements to the UX in exercise-session component
    - Added 'Add Next' text button
    - Disabled the remove icon for sets if there is only 1 set in the exercise

--- src/app/features/exercise/components/exercise-session/exercise-session.ts
+++ src/app/features/exercise/components/exercise-session/exercise-session.ts
@@ -1,7 +1,7 @@
 import { Component, effect, inject } from '@angular/core';
 import { ReactiveFormsModule, ɵInternalFormsSharedModule } from '@angular/forms';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { faSolidTrash } from '@ng-icons/font-awesome/solid';
+import { faSolidBackward, faSolidBackwardStep, faSolidTrash } from '@ng-icons/font-awesome/solid';
 import { ExerciseModalLayoutService } from '../../services/exercise-modal-layout-service';
 import { ExerciseSessionService } from '../../services/exercise-session-service';
 import { ExerciseType } from '../../../workout/models/exercise-type';
@@ -14,7 +14,7 @@ import { Router } from '@angular/router';
     imports: [ɵInternalFormsSharedModule, ReactiveFormsModule, NgIcon, Button],
     templateUrl: './exercise-session.html',
     styleUrl: './exercise-session.css',
-    providers: [provideIcons({ faSolidTrash })]
+    providers: [provideIcons({ faSolidTrash, faSolidBackwardStep })]
 })
 export class ExerciseSession {
     modalConfig = inject(ExerciseModalLayoutService);
@@ -35,7 +35,7 @@ export class ExerciseSession {
             const exercises = this.exercises() as [];
 
             if(exercises?.length === 0)
-                this.router.navigate(['workout-form/exercises']);
+                this.backToExerciseList();
         })
 
     }
@@ -54,10 +54,16 @@ export class ExerciseSession {
     }
 
     removeSet(exerciseIndex: number, setIndex: number) {
+        if(this.exerciseSession.getExerciseDetails(exerciseIndex).length <= 1) return;
+
         this.exerciseSession.removeDetails(exerciseIndex, setIndex);
     }
 
     removeExercise(exerciseIndex: number) {
         this.exerciseSession.removeExercise(exerciseIndex);
+    }
+
+    backToExerciseList() {
+        this.router.navigate(['workout-form/exercises']);
     }
 }
