7c3ea04a27c0bac8ff86d77fc8a04551364728b0
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-04-02 17:33:53 +0200

    refactor(workout-page-dto): restructure WorkoutPageDto to include year, month, and available months/years

--- src/app/features/workout/models/workout-page-dto.ts
+++ src/app/features/workout/models/workout-page-dto.ts
@@ -1,8 +1,11 @@
-import { PagedResult } from "../../../core/models/PagedResult"
 import { WorkoutListItemDto } from "./workout-list-item-dto"
 import { WorkoutSummaryDto } from "./workout-summary-dto"
 
 export type WorkoutPageDto = {
-    pagedWorkouts: PagedResult<WorkoutListItemDto>,
+    year: number | null,
+    month: number | null,
+    availableYears: number[],
+    availableMonths: number[],
+    workouts: WorkoutListItemDto[],
     workoutSummary: WorkoutSummaryDto
 }
