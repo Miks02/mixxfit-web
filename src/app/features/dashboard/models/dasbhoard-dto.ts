a39fe5e06f73e275c75d64a575076ace133f5604
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-04-26 10:29:51 +0200

    refactor(dashboard): Remove unused properties from DashboardDto

--- src/app/features/dashboard/models/dasbhoard-dto.ts
+++ src/app/features/dashboard/models/dasbhoard-dto.ts
@@ -1,9 +1,6 @@
 import { WorkoutListItemDto } from "../../workout/models/workout-list-item-dto"
 
 export type DashboardDto = {
-    dailyCalories: number,
-    averageSleep: number,
-    waterIntake: number,
     lastWorkoutDate: string,
     workoutStreak: number,
     recentWorkouts: WorkoutListItemDto[]
