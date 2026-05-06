3784db900330dd3d8d756af72c5ab0d69d742fd3
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-05-06 12:20:44 +0200

    feat(template-factories): Add functions to map exercises and create template requests

--- src/app/features/templates/factories/template-factories.ts
+++ src/app/features/templates/factories/template-factories.ts
@@ -1,4 +1,6 @@
 import {NonNullableFormBuilder, Validators } from "@angular/forms";
+import { TemplateExerciseDto } from "../models/template-exercise-dto";
+import { TemplateRequest } from "../models/template-request";
 
 
 export function createCurrentTemplate(fb: NonNullableFormBuilder) {
@@ -6,4 +8,16 @@ export function createCurrentTemplate(fb: NonNullableFormBuilder) {
         name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
         exercises: fb.array<Omit<TemplateExerciseView, 'isUserDefined' | 'order'>>([], [Validators.required])
     })
+}
+
+export function mapTemplateExercises(exercises: Omit<TemplateExerciseView, 'isUserDefined' | 'order'>[]): TemplateExerciseDto[] {
+    return exercises.map(e => ({exerciseId: e.exerciseId, setCount: e.setCount}))
+}
+
+export function createTemplateRequestFromForm(name: string, exercises: TemplateExerciseDto[], notes?: string): TemplateRequest {
+    return {
+        name: name,
+        notes: notes,
+        exercises: exercises
+    }
 }
