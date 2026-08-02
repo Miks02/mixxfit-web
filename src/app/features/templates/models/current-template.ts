40f81aebd77eb47bd430f65a9140bbbd40ab17c9
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-02 10:32:05 +0200

    fix(current-template): Import TemplateExerciseView for type consistency

--- src/app/features/templates/models/current-template.ts
+++ src/app/features/templates/models/current-template.ts
@@ -1,3 +1,5 @@
+import { TemplateExerciseView } from "./template-exercise-view"
+
 export type CurrentTemplate = {
     name: string,
     exercises: Omit<TemplateExerciseView, 'isUserDefined' | 'order'>[]
