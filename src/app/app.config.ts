3a6ec462a1db77d574ee4d94e3b47bebf25d5be2
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-05 20:16:03 +0200

    chore(app-config): Install tanstack query and configure providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,23 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { httpErrorInterceptor } from './core/interceptors/http-error-interceptor';
+import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([
-        httpErrorInterceptor, authInterceptor
-    ])), provideCharts(withDefaultRegisterables())
-  ]
+    providers: [
+        provideTanStackQuery(new QueryClient({
+            defaultOptions: {
+                queries: {
+                    staleTime: 60 * 1000,
+                    retry: 1
+                }
+            }
+        })),
+        provideBrowserGlobalErrorListeners(),
+        provideRouter(routes),
+        provideHttpClient(withInterceptors([
+            httpErrorInterceptor, authInterceptor
+        ])),
+        provideCharts(withDefaultRegisterables())
+    ]
 };
