7ac3dc1528038d2f4e4efcb5f309491286cf60de
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-07 01:46:46 +0200

    refactor(auth): Replace behaviour subject with signals in auth-service and clear up unnecesary methods

--- src/app/core/services/auth-service.ts
+++ src/app/core/services/auth-service.ts
@@ -1,12 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { inject } from '@angular/core';
-import { RegisterRequest } from '../../features/auth/models/register-request';
-import { AuthResponse } from '../../features/auth/models/auth-response';
-import { Observable, map, tap } from 'rxjs';
-import { LoginRequest } from '../../features/auth/models/login-request';
+import { inject, Injectable, Signal, signal, WritableSignal } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { AuthResponse } from '../../features/auth/models/auth-response';
+import { LoginRequest } from '../../features/auth/models/login-request';
+import { RegisterRequest } from '../../features/auth/models/register-request';
 import { UserState } from '../states/user-state';
 
 @Injectable({
@@ -14,15 +12,14 @@ import { UserState } from '../states/user-state';
 })
 export class AuthService {
     private readonly api: string = environment.apiUrl;
-    private accessTokenSubject = new BehaviorSubject<string | null>(localStorage.getItem('token'));
-    public accessToken$ = this.accessTokenSubject.asObservable();
+    private _accessToken: WritableSignal<string | null> = signal(localStorage.getItem('token'));
+    public accessToken: Signal<string | null> = this._accessToken.asReadonly();
 
     private readonly http = inject(HttpClient)
     private userState = inject(UserState);
 
-    get accessToken(): string | null {return this.accessTokenSubject.value}
-    set accessToken(accessToken: string | null) {
-        this.accessTokenSubject.next(accessToken)
+    setAccessToken(accessToken: string | null) {
+        this._accessToken.set(accessToken);
 
         if(accessToken === null)
             localStorage.removeItem('token')
@@ -30,15 +27,11 @@ export class AuthService {
             localStorage.setItem('token', accessToken as string)
     }
 
-    constructor() {
-        this.accessTokenSubject.next(this.accessToken)
-    }
-
     register(model: RegisterRequest): Observable<AuthResponse> {
         return this.http.post<AuthResponse>(`${this.api}/auth/register`, model, {withCredentials: true})
         .pipe(
             tap(res => {
-                this.accessToken = res.accessToken;
+                this.setAccessToken(res.accessToken);
                 this.userState.setUserDetails(res.user);
             })
         )
@@ -48,7 +41,7 @@ export class AuthService {
         return this.http.post<AuthResponse>(`${this.api}/auth/login`, model , {withCredentials: true})
         .pipe(
             tap(res => {
-                this.accessToken = res.accessToken;
+                this.setAccessToken(res.accessToken);
                 this.userState.setUserDetails(res.user);
             })
         )
@@ -70,21 +63,15 @@ export class AuthService {
         )
     }
 
-    test(): Observable<void> {
-        return this.http.get<void>(`${this.api}/auth/test`).pipe(
-            tap(res => console.log(res))
-        );
-    }
-
     isAuthenticated() {
-        if(this.accessToken) return true;
+        if(this.accessToken()) return true;
         return false;
     }
 
     clearAuthData() {
         this.userState.resetCurrentUser();
         localStorage.removeItem('token');
-        this.accessTokenSubject.next(null);
+        this.setAccessToken(null);
     }
 
 }
