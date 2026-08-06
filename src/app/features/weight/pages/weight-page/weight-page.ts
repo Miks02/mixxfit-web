3364a38bf97244f637bf6b19a2763db0dd33706d
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-06 21:40:39 +0200

    refactor(weight-page): Remove 'dead' code from weight-page component

--- src/app/features/weight/pages/weight-page/weight-page.ts
+++ src/app/features/weight/pages/weight-page/weight-page.ts
@@ -2,6 +2,7 @@ import { DatePipe, DecimalPipe, SlicePipe } from "@angular/common";
 import { afterNextRender, Component, computed, effect, ElementRef, inject, Signal, signal, viewChildren, WritableSignal } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from "@angular/router";
+import { WeightRecord } from "@features/weight/models/weight-record";
 import { NgIcon, provideIcons } from "@ng-icons/core";
 import {
     faSolidBullseye,
@@ -15,17 +16,16 @@ import {
     faSolidScaleUnbalanced,
     faSolidWeightScale
 } from "@ng-icons/font-awesome/solid";
+import { Button, Modal, ModalData, ModalType } from '@shared';
 import { NgxSkeletonLoaderComponent } from 'ngx-skeleton-loader';
 import { take } from 'rxjs';
 import { isControlValid } from '../../../../core/helpers/form-helpers';
 import { formatDate } from '../../../../core/helpers/utility';
-import { ModalData, ModalType, Button, Modal } from '@shared';
 import { NotificationService } from '../../../../core/services/notification-service';
 import { UserState } from '../../../../core/states/user-state';
-import { LayoutState } from '../../../../layout/services/layout-state';``
+import { LayoutState } from '../../../../layout/services/layout-state';
 import { WeightChart } from "../../components/weight-chart/weight-chart";
 import { createTargetWeightForm, createWeightEntryForm } from "../../factories/weight-form-factories";
-import { WeightEntryDetails } from '../../models/weight-entry-details';
 import { WeightEntryService } from '../../services/weight-entry-service';
 
 @Component({
@@ -47,10 +47,8 @@ export class WeightPage  {
     private activatedroute = inject(ActivatedRoute);
 
     isModalOpen = signal(false);
-    selectedWeightEntry: WritableSignal<WeightEntryDetails | null> = signal(null);
+    selectedWeightEntry: WritableSignal<WeightRecord | null> = signal(null);
     user = this.userState.userDetails;
-    // weightSummary = this.weightService.weightSummary;
-    // weightListDetails = this.weightService.weightListDetails;
 
     form = createWeightEntryForm(this.fb);
     targetWeightForm = createTargetWeightForm(this.fb);
@@ -80,8 +78,6 @@ export class WeightPage  {
             const month = this.selectedMonth()
             const year = this.selectedYear()
 
-            console.log("Mesec: ", month, "Godina: ", year)
-
             this.router.navigate([], {
                 relativeTo: this.activatedroute,
                 queryParams: {
@@ -99,76 +95,19 @@ export class WeightPage  {
         });
     }
 
-    // ngOnInit() {
-    //     this.activatedroute.queryParams
-    //     .pipe(take(1))
-    //     .subscribe((params) => {
-    //         this.selectedMonth.set(params['month'] ? +params['month'] : null)
-    //         this.selectedYear.set(params['year'] ? +params['year'] : null)
-    //         this.loadWeightSummary();
-    //     })
-    // }
-
-    // loadWeightSummary() {
-    //     this.weightService.getMyWeightSummary(this.selectedMonth(), this.selectedYear(), this.targetWeight())
-    //     .pipe(take(1))
-    //     .subscribe(() => {
-    //         this.syncFilters();
-    //     })
-    // }
-
-    // private syncFilters() {
-    //     const availableMonths = this.months();
-    //     const availableYears = this.years();
-    //     const currentMonth = this.selectedMonth();
-    //     const currentYear = this.selectedYear();
-    //     let filtersChanged = false;
-
-    //     if (currentYear !== null && availableYears && !availableYears.includes(currentYear)) {
-    //         this.selectedYear.set(availableYears.length > 0 ? availableYears[0] : null);
-    //         filtersChanged = true;
-    //     }
-
-    //     if (currentMonth !== null && availableMonths && !availableMonths.includes(currentMonth)) {
-    //         this.selectedMonth.set(availableMonths.length > 0 ? availableMonths[0] : null);
-    //         filtersChanged = true;
-    //     }
-
-    //     if (filtersChanged) {
-    //         this.loadWeightLogs();
-    //     }
-    // }
-
-    // loadWeightLogs() {
-    //     this.weightService.getMyWeightLogs(this.selectedMonth(), this.selectedYear())
-    //     .pipe(take(1))
-    //     .subscribe()
-    // }
-
-    // loadWeightChart(targetWeight: number | null = null) {
-    //     this.weightService.getMyWeightChart(targetWeight ?? this.targetWeight())
-    //     .pipe(take(1))
-    //     .subscribe()
-    // }
+    ngOnInit() {
+        this.activatedroute.queryParams
+        .pipe(take(1))
+        .subscribe((params) => {
+            this.selectedMonth.set(params['month'] ? +params['month'] : null)
+            this.selectedYear.set(params['year'] ? +params['year'] : null)
+        })
+    }
 
     onSubmit() {
         if(this.form.invalid)
             return;
 
-        // this.weightService.addWeightEntry(this.form.value)
-        // .pipe(take(1))
-        // .subscribe({
-        //     next: () => {
-        //         this.form.reset();
-        //         this.notificationService.showSuccess("Weight logged successfully");
-        //         //this.loadWeightSummary();
-        //     },
-        //     error: (err) => {
-        //          if(err.error.errorCode === "WeightEntry.LimitReached")
-        //             this.notificationService.showInfo("You can only log weight once per day")
-        //     }
-        // });
-        //
         this.weightService.addWeightEntryMutation.mutate(this.form.value, {
             onSuccess: () => {
                 this.form.reset();
@@ -180,6 +119,10 @@ export class WeightPage  {
                     return;
                 }
                 this.notificationService.showSuccess("New weight entry saved. Update your filters");
+            },
+            onError: (err) => {
+                if(err.errorCode === "WeightEntry.LimitReached")
+                    this.notificationService.showInfo("You can only log weight once per day")
             }
         });
 
@@ -201,14 +144,10 @@ export class WeightPage  {
     saveTargetWeight() {
         if(this.targetWeightForm.invalid)
             return;
-
-        this.weightService.updateTargetWeight(this.targetWeightForm.value)
-        .pipe(take(1))
-        .subscribe({
-            next: (res) => {
+        this.weightService.updateTargetWeightMutation.mutate(this.targetWeightForm.value, {
+            onSuccess: () => {
                 this.notificationService.showSuccess("Target weight updated successfully");
                 this.isTargetFormOpen.set(false);
-                //this.loadWeightChart(res.targetWeight);
             }
         });
     }
@@ -219,13 +158,15 @@ export class WeightPage  {
     }
 
     loadWeightEntry(id: number) {
-        this.weightService.getMyWeightLog(id)
-        .pipe(take(1))
-        .subscribe((res) => {
-            this.selectedWeightEntry.set(res)
-            this.isModalOpen.set(true);
-        })
+        const logs = this.weightLogs();
 
+        const selectedLog = logs?.find((l) => l.id == id);
+        if (!selectedLog) {
+            this.notificationService.showError("Requested weight log not found. Please try again");
+            return;
+        }
+        this.selectedWeightEntry.set(selectedLog);
+        this.isModalOpen.set(true);
     }
 
     deleteWeightEntry() {
@@ -233,17 +174,12 @@ export class WeightPage  {
         if(!selected)
             return;
 
-        this.weightService.deleteWeightEntry(selected.id)
-        .pipe(take(1))
-        .subscribe(() => {
-            //this.loadWeightSummary()
-            this.isModalOpen.set(false);
-            this.notificationService.showSuccess("Weight log has been deleted successfully")
-        })
-    }
-
-    closeModal() {
-        this.isModalOpen.set(false);
+        this.weightService.deleteWeightEntryMutation.mutate(selected.id, {
+            onSuccess: () => {
+                this.isModalOpen.set(false);
+                this.notificationService.showSuccess("Weight log has been deleted successfully")
+            }
+        });
     }
 
     getTargetWeightMessage = computed(() => {
@@ -255,10 +191,9 @@ export class WeightPage  {
     buildModal = computed((): ModalData => {
         const entry = this.selectedWeightEntry();
         const entryDate = formatDate(entry?.createdAt!)
-        const notes = entry?.notes ? `| ${entry.notes}` : ""
 
         return {
-            title: `${entry?.weight} KG | ${entry?.time.substring(0, 5)} | ${entryDate} ${notes}`,
+            title: `${entry?.weight} KG | ${entry?.timeLogged.substring(0, 5)} | ${entryDate}`,
             subtitle: `You are about to delete this weight entry. This action cannot be undone.`,
             type: ModalType.Warning,
             primaryActionLabel: 'Confirm',
