3f03c84a3798c988a83501e260687f96308cda9c
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-13 19:46:45 +0200

    wip(profile-service): Integrate TanStack Query for user profile mutations and enhance state management
    - Breaking changes

--- src/app/features/profile/services/profile-service.ts
+++ src/app/features/profile/services/profile-service.ts
@@ -1,16 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { tap } from 'rxjs';
+import { injectMutation } from '@tanstack/angular-query-experimental';
+import { lastValueFrom } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { Gender } from '../../../core/models/gender';
+import { ProblemDetails } from '../../../core/models/problem-details';
+import { UserState } from '../../../core/states/user-state';
 import { DateOfBirthDto } from '../models/date-of-birth-dto';
+import { EmailDto } from '../models/email-dto';
+import { FullNameDto } from '../models/full-name-dto';
 import { GenderDto } from '../models/gender-dto';
 import { HeightDto } from '../models/height-dto';
-import { UserState } from '../../../core/states/user-state';
-import { FullNameDto } from '../models/full-name-dto';
-import { EmailDto } from '../models/email-dto';
-import { UserNameDto } from '../models/username-dto';
 import { PasswordDto } from '../models/password-dto';
+import { UserNameDto } from '../models/username-dto';
 
 @Injectable({
     providedIn: 'root',
@@ -22,87 +24,110 @@ export class ProfileService {
     private http = inject(HttpClient);
     private userState = inject(UserState);
 
-    deleteAccount() {
+    deleteAccountMutation = injectMutation<void, ProblemDetails, void>(() => ({
+        mutationFn: async () => await lastValueFrom(this.deleteAccount()),
+    }));
+
+    deleteProfilePictureMutation = injectMutation<void, ProblemDetails, void>(() => ({
+        mutationFn: async () => await lastValueFrom(this.deleteProfilePicture()),
+        onSuccess: () => {
+            this.userState.updateUserDetails({imagePath: null});
+        }
+    }));
+
+    changePasswordMutation = injectMutation<void, ProblemDetails, PasswordDto>(() => ({
+        mutationFn: async (model: PasswordDto) => await lastValueFrom(this.changePassword(model)),
+    }));
+
+    updateFullNameMutation = injectMutation<FullNameDto, ProblemDetails, FullNameDto>(() => ({
+        mutationFn: async (fullName: FullNameDto) => await lastValueFrom(this.updateFullName(fullName)),
+        onSuccess: (res) => {
+            this.userState.updateUserDetails({fullName: res.firstName + ' ' + res.lastName});
+        }
+    }));
+
+    updateUserNameMutation = injectMutation<UserNameDto, ProblemDetails, UserNameDto>(() => ({
+        mutationFn: async (username: UserNameDto) => await lastValueFrom(this.updateUserName(username)),
+        onSuccess: (res) => {
+            this.userState.updateUserDetails({userName: res.userName});
+        }
+    }));
+
+    updateEmailMutation = injectMutation<EmailDto, ProblemDetails, EmailDto>(() => ({
+        mutationFn: async (email: EmailDto) => await lastValueFrom(this.updateEmail(email)),
+        onSuccess: (res) => {
+            this.userState.updateUserDetails({email: res.email});
+        }
+    }));
+
+    updateProfilePictureMutation = injectMutation<string, ProblemDetails, File>(() => ({
+        mutationFn: async (imageFile: File) => await lastValueFrom(this.updateProfilePicture(imageFile)),
+        onSuccess: (res) => {
+            this.userState.updateUserDetails({imagePath: this.urlOnly + res});
+        }
+    }));
+
+    updateDateOfBirthMutation = injectMutation<string, ProblemDetails, DateOfBirthDto>(() => ({
+        mutationFn: async (dob: DateOfBirthDto) => await lastValueFrom(this.updateDateOfBirth(dob)),
+        onSuccess: (res) => {
+            this.userState.updateUserDetails({dateOfBirth: res});
+        }
+    }));
+
+    updateGenderMutation = injectMutation<Gender, ProblemDetails, GenderDto>(() => ({
+        mutationFn: async (gender: GenderDto) => await lastValueFrom(this.updateGender(gender)),
+        onSuccess: (res) => {
+            this.userState.updateUserDetails({gender: res});
+        }
+    }));
+
+    updateHeightMutation = injectMutation<number, ProblemDetails, HeightDto>(() => ({
+        mutationFn: async (height: HeightDto) => await lastValueFrom(this.updateHeight(height)),
+        onSuccess: (res) => {
+            this.userState.updateUserDetails({height: res});
+        }
+    }));
+
+    private deleteAccount() {
         return this.http.delete<void>(`${this.api}/users`)
     }
 
-    deleteProfilePicture() {
+    private deleteProfilePicture() {
         return this.http.delete<void>(`${this.api}/users/profile-picture`)
-        .pipe(
-            tap(() => {
-                this.userState.updateUserDetails({imagePath: null});
-            })
-        )
     }
 
-    changePassword(model: PasswordDto){
+    private changePassword(model: PasswordDto){
         return this.http.post<void>(`${this.api}/auth/password`, model)
     }
 
-    updateFullName(fullName: FullNameDto) {
-        return this.http.patch<FullNameDto>(`${this.api}/users/fullname`, fullName )
-        .pipe(
-            tap((res) => {
-                this.userState.updateUserDetails({fullName: res.firstName + ' ' + res.lastName})
-            })
-        )
+    private updateFullName(fullName: FullNameDto) {
+        return this.http.patch<FullNameDto>(`${this.api}/users/fullname`, fullName)
     }
 
-    updateUserName(username: UserNameDto) {
-        return this.http.patch<UserNameDto>(`${this.api}/users/username`, username,)
-        .pipe(
-            tap(res => {
-                this.userState.updateUserDetails({userName: res.userName});
-            })
-        );
+    private updateUserName(username: UserNameDto) {
+        return this.http.patch<UserNameDto>(`${this.api}/users/username`, username)
     }
 
-    updateEmail(email: EmailDto) {
-        return this.http.patch<EmailDto>(`${this.api}/users/email`, email,)
-        .pipe(
-            tap(res => {
-                this.userState.updateUserDetails({email: res.email});
-            })
-        );
-
+    private updateEmail(email: EmailDto) {
+        return this.http.patch<EmailDto>(`${this.api}/users/email`, email)
     }
 
-    updateProfilePicture(imageFile: File) {
+    private updateProfilePicture(imageFile: File) {
         const formData = new FormData();
         formData.append('imageFile', imageFile, imageFile.name);
 
         return this.http.patch(`${this.api}/users/profile-picture`, formData, {responseType: 'text'})
-        .pipe(
-            tap(res => {
-                this.userState.updateUserDetails({imagePath: this.urlOnly + res});
-            })
-        )
     }
 
-    updateDateOfBirth(dob: DateOfBirthDto) {
+    private updateDateOfBirth(dob: DateOfBirthDto) {
         return this.http.patch<string>(`${this.api}/fitness-profile/date-of-birth`, dob)
-        .pipe(
-            tap(res => {
-                this.userState.updateUserDetails({dateOfBirth: res});
-            })
-        );
     }
 
-    updateGender(gender: GenderDto) {
+    private updateGender(gender: GenderDto) {
         return this.http.patch<Gender>(`${this.api}/fitness-profile/gender`, gender)
-        .pipe(
-            tap(res => {
-                this.userState.updateUserDetails({gender: res});
-            })
-        );
     }
 
-    updateHeight(height: HeightDto) {
+    private updateHeight(height: HeightDto) {
         return this.http.patch<number>(`${this.api}/fitness-profile/height`, height)
-        .pipe(
-            tap(res => {
-                this.userState.updateUserDetails({height: res});
-            })
-        );
     }
 }
