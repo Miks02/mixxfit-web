956d68fd0f9b316699916895e93c1ab2ff2be2ee
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-14 13:13:01 +0200

    feat(tanstack): Set staleTime to infinity and and extend the retry logic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,14 +6,21 @@ import { authInterceptor } from './core/interceptors/auth-interceptor';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { httpErrorInterceptor } from './core/interceptors/http-error-interceptor';
 import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
+import { ProblemDetails } from './core/models/problem-details';
+
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideTanStackQuery(new QueryClient({
             defaultOptions: {
                 queries: {
-                    staleTime: 5 * 60 * 1000,
-                    retry: false
+                    staleTime: Infinity,
+                    retry: (failureCount, error: any) => {
+                        const errorStatus = error.status;
+                        if(errorStatus >= 400 && errorStatus < 500)
+                            return false;
+                        return failureCount < 3;
+                    }
                 }
             }
         })),
