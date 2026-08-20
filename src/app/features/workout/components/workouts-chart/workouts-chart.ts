44e646a09101805c519ac5452dba838d85e403b2
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-20 16:15:58 +0200

    wip(workout-chart): Begin a migration process from ng2-charts to apexcharts for workout chart
    - Breaking changes

--- src/app/features/workout/components/workouts-chart/workouts-chart.ts
+++ src/app/features/workout/components/workouts-chart/workouts-chart.ts
@@ -1,34 +1,73 @@
-import { Component, computed, Input, signal, Signal } from '@angular/core';
-import {
-    Chart,
-    ChartConfiguration,
-    ChartOptions,
-    registerables
-} from 'chart.js';
+import { Component, computed, input, Input, NgModule, signal, Signal } from '@angular/core';
 import { BaseChartDirective } from 'ng2-charts';
 import { WorkoutsPerMonthDto } from '../../models/workouts-per-month-dto';
-Chart.register(...registerables)
+import {
+    ChartComponent,
+    ApexAxisChartSeries,
+    ApexNonAxisChartSeries,
+    ApexChart,
+    ApexXAxis,
+    ApexYAxis,
+    ApexTitleSubtitle,
+    ApexDataLabels,
+    ApexStroke,
+    ApexFill,
+    ApexLegend,
+    ApexTooltip,
+    ApexMarkers,
+    ApexPlotOptions,
+    ApexResponsive,
+    ApexGrid,
+    ApexAnnotations,
+    ApexStates,
+    ApexTheme,
+    NgApexchartsModule,
+} from 'ng-apexcharts';
+import { NgIcon } from '@ng-icons/core';
+import { NgxSkeletonLoaderComponent } from 'ngx-skeleton-loader';
+
+export type ChartOptions = {
+    series?: ApexAxisChartSeries | ApexNonAxisChartSeries;
+    chart?: ApexChart;
+    xaxis?: ApexXAxis;
+    yaxis?: ApexYAxis | ApexYAxis[];
+    title?: ApexTitleSubtitle;
+    subtitle?: ApexTitleSubtitle;
+    dataLabels?: ApexDataLabels;
+    stroke?: ApexStroke;
+    fill?: ApexFill;
+    legend?: ApexLegend;
+    tooltip?: ApexTooltip;
+    markers?: ApexMarkers;
+    plotOptions?: ApexPlotOptions;
+    responsive?: ApexResponsive[];
+    grid?: ApexGrid;
+    annotations?: ApexAnnotations;
+    states?: ApexStates;
+    theme?: ApexTheme;
+    colors?: string[];
+    labels?: any;
+};
 
 @Component({
     selector: 'app-workouts-chart',
-    imports: [BaseChartDirective],
+    imports: [BaseChartDirective, NgApexchartsModule, NgIcon, NgxSkeletonLoaderComponent],
     templateUrl: './workouts-chart.html',
     styleUrl: './workouts-chart.css',
+    host: {
+        class: 'flex flex-col min-w-75',
+    },
 })
 export class WorkoutsChart {
-    public barChartType: 'bar' = "bar";
+    workoutCountsSource = input.required<WorkoutsPerMonthDto | undefined>();
+    availableYears = input.required<number[] | undefined>();
 
-    @Input()
-    workoutCountsSource: Signal<WorkoutsPerMonthDto | undefined> = signal(undefined)
-
-    public barChartData = computed<ChartConfiguration<'bar'>['data']>(() => {
+    chartOptions = computed<Partial<ChartOptions>>(() => {
         const data = this.workoutCountsSource();
-
         return {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            datasets: [
+            series: [
                 {
-                    label: 'Workouts',
+                    name: 'Workouts',
                     data: [
                         data?.januaryWorkouts ?? 0,
                         data?.februaryWorkouts ?? 0,
@@ -41,44 +80,80 @@ export class WorkoutsChart {
                         data?.septemberWorkouts ?? 0,
                         data?.octoberWorkouts ?? 0,
                         data?.novemberWorkouts ?? 0,
-                        data?.decemberWorkouts ?? 0
+                        data?.decemberWorkouts ?? 0,
                     ],
-                    borderRadius: 6,
-                    barPercentage: 1,
-                    categoryPercentage: 0.8,
-                    backgroundColor: 'rgba(235, 170, 11, 1)'
-                }
-            ]
+                },
+            ],
+            chart: {
+                type: 'bar',
+                height: 250,
+
+                toolbar: {
+                    show: false,
+                },
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: '85%',
+                    borderRadius: 5,
+                    borderRadiusApplication: 'end',
+                },
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent'],
+            },
+            xaxis: {
+                categories: [
+                    'Jan',
+                    'Feb',
+                    'Mar',
+                    'Apr',
+                    'May',
+                    'Jun',
+                    'Jul',
+                    'Aug',
+                    'Sep',
+                    'Oct',
+                    'Nov',
+                    'Dec',
+                ],
+                labels: {
+                    style: {
+                        fontSize: '12px',
+                    },
+                },
+            },
+            fill: {
+                opacity: 1,
+                colors: ['orange'],
+            },
+            responsive: [
+                {
+                    breakpoint: 600,
+                    options: {
+                        chart: {
+                            height: 250,
+                        },
+                        plotOptions: {
+                            bar: {
+                                columnWidth: '90%',
+                                borderRadius: 5,
+                            },
+                        },
+                        xaxis: {
+                            labels: {
+                                rotate: -45,
+                                style: {
+                                    fontSize: '10px',
+                                },
+                            },
+                        },
+                    },
+                },
+            ],
         };
     });
-
-    public barChartOptions: ChartOptions<'bar'> = {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-            legend: {
-                display: true,
-                position: 'bottom'
-            },
-            tooltip: {
-                enabled: true
-            }
-        },
-        scales: {
-            x: {
-                ticks: {
-                    display: true,
-                },
-                grid: {
-                    display: false
-                }
-            },
-            y: {
-                beginAtZero: true,
-                ticks: {
-                    stepSize: 2
-                }
-            }
-        }
-    };
 }
